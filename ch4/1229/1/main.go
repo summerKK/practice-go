da1d3d36f9a08704d91219680a6a1b6e6c1194c8
summer <su943515688@gmail.com>
2018-01-02 21:05:15 +0800

    hello,2018

--- ch4/1229/1/main.go
+++ ch4/1229/1/main.go
@@ -8,10 +8,16 @@ import (
 func main() {
 	attrJson := map[string]string{}
 	attr := `{"Brand":"Hermès","Condition":"New or Never Worn","Designer":"Hermès","Dimensions":"24 cm Hx30 cm Wx15 cm D","Material Notes":"Leather","Period":"21st Century","Place of Origin":"France","Seller Location":"New York, NY"}`
-	err := json.Unmarshal([]byte(attr),&attrJson)
-	if err != nil{
+	err := json.Unmarshal([]byte(attr), &attrJson)
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	attrJson1 := map[string]string{}
+	attr1 := ``
+	json.Unmarshal([]byte(attr1), &attrJson1)
 	fmt.Println(attrJson)
+	fmt.Println(attrJson1)
+	fmt.Println(len(attrJson1))
 }
